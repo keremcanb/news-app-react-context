7379f18d381da23d0773b67c4865ea80a0c115bb
keremcanb <keremcan@gmail.com>
2021-02-22 21:52:46 +0300

    m

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -9,7 +9,7 @@ const SearchPage = () => {
   useEffect(() => {
     const delayDebounce = setTimeout(() => {
       searchArticles(query, 12, page, sort);
-    }, 500);
+    }, 1000);
     return () => clearTimeout(delayDebounce);
   }, [query, page, sort]);
 
@@ -17,14 +17,14 @@ const SearchPage = () => {
     return <Loader />;
   }
   if (results.length < 1) {
-    return <Error text="No Results Found" />;
+    return <Error text='No Results Found' />;
   }
   return (
     <>
       <Helmet>
         <title>Search results</title>
       </Helmet>
-      <PageHero title="Search results" isBookmark isSort />
+      <PageHero title='Search results' isBookmark isSort />
       <Pagination />
       <ArticleGrid articles={results} />
       <Pagination />
